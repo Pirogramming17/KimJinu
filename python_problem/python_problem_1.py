ffa2219917a76f35e16f4e781d0379ae2d4b60c1
Jinu-Kim <jinu053@naver.com>
2022-07-08 19:23:51 +0900

    1-7

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,36 +2,50 @@
 num = 0
 i =1
 sum = 0
+order =1 
 """단게 1.2 (수정)"""
 """단게 1.5(수정)"""
 """단게 1.6(수정)"""
-while sum <31:
-    num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")
-    if num.isdigit():
-        if (int(num) in (1,2,3)):
-            num = int(num)
-            for i in range(int(num)):
-                print("playerA: %d \n" %(sum+i+1))
-                sum += i
-                if sum == 31:
-                    break
-        else:
-            print("1,2,3 중 하나를 입력하세요. \n")
-    else:
-        print("정수를 입력하세요. \n")
+"""단게 1.7(수정)"""
 
-    num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")    
-    if num.isdigit():
-        if (int(num) in (1,2,3)):
-            num = int(num)
-            for i in range(int(num)):
-                print("playerB: %d \n" %(sum+i+1))
-                sum = sum+i
-                if sum == 31:
-                    break
+while sum <31:
+    if sum ==31:
+        break
+    if order %2 !=0:
+        num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")
+        if num.isdigit():
+            if (int(num) in (1,2,3)):
+                num = int(num)
+                for i in range(int(num)):
+                    print("playerA: %d \n" %(sum+i+1))
+                for i in range(int(num)):
+                    sum += 1
+                    if sum == 31:
+                        win="B"
+                        break
+            else:
+                print("1,2,3 중 하나를 입력하세요. \n")
         else:
-            print("1,2,3 중 하나를 입력하세요. \n") 
+            print("정수를 입력하세요. \n")
+        order +=1
     else:
-        print("정수를 입력하세요. \n")
-    
+        num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")    
+        if num.isdigit():
+            if (int(num) in (1,2,3)):
+                num = int(num)
+                for i in range(int(num)):
+                    print("playerB: %d \n" %(sum+i+1))
+                for i in range(int(num)):
+                    sum += 1
+                    if sum == 31:
+                        win ="A"
+                        break
+            else:
+                print("1,2,3 중 하나를 입력하세요. \n") 
+        else:
+            print("정수를 입력하세요. \n")
+        order -=1
+
+print("Player"+win+"win!\n")
+
     
