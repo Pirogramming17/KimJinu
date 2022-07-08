3055c90d4cf56077821bd1f4380099cf4a88506f
Jinu-Kim <jinu053@naver.com>
2022-07-08 21:35:31 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,51 +1,50 @@
 """단계 1.1"""
-num = 0
-i =1
-sum = 0
-order =1 
 """단게 1.2 (수정)"""
 """단게 1.5(수정)"""
 """단게 1.6(수정)"""
 """단게 1.7(수정)"""
+"""단게 1.8(수정)"""
+"""단게 1.7(수정)"""
+"""단계 1.8"""
+"""단계 1.9"""
+import random
+order = random.randrange(0,2) 
+total = list(range(1, 32)) 
+count = list(range(1, 4)) 
 
-while sum <31:
-    if sum ==31:
+i = 0
+
+while i <= 31:    
+    
+    if i == 31 : 
         break
-    if order %2 !=0:
-        num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")
-        if num.isdigit():
-            if (int(num) in (1,2,3)):
-                num = int(num)
-                for i in range(int(num)):
-                    print("playerA: %d \n" %(sum+i+1))
-                for i in range(int(num)):
-                    sum += 1
-                    if sum == 31:
-                        win="B"
-                        break
-            else:
-                print("1,2,3 중 하나를 입력하세요. \n")
-        else:
-            print("정수를 입력하세요. \n")
-        order +=1
+    
+    if order % 2 == 0:
+        num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : "))
+        while num not in range(0,4):
+            print('1,2,3중 하나를 입력하세요.\n')
+            num = int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능)"))
+        
+        for count in range(num):
+            i += 1
+           
+            print("player"+str(i))
+            if i == 31:
+                print("Computer Win!")
+                break  
+    
+       
     else:
-        num = input("부를 숫자의 개수를 입력하세요(1,2,3만 입력가능) :")    
-        if num.isdigit():
-            if (int(num) in (1,2,3)):
-                num = int(num)
-                for i in range(int(num)):
-                    print("playerB: %d \n" %(sum+i+1))
-                for i in range(int(num)):
-                    sum += 1
-                    if sum == 31:
-                        win ="A"
-                        break
-            else:
-                print("1,2,3 중 하나를 입력하세요. \n") 
-        else:
-            print("정수를 입력하세요. \n")
-        order -=1
-
-print("Player"+win+"win!\n")
+        computer = random.randrange(1,4)
+        
+        for count in range(computer):
+            i += 1
+            
+            print("computer"+str(i))
+            if i == 31:
+                print("Player Win!")  
+                break
+    
+    order += 1
 
     
